dbe22ac1fc9054efc12865e2619f08b9431d2004
kalaiselvankadirvel <kalaiselvan@outlook.com>
2019-07-25 20:26:16 -0400

    Initial Upload

---
+++ spring-security-backend/src/main/java/in/kalaiselvan/springsecurity/SpringSecurityBackendApplication.java
@@ -0,0 +1,13 @@
+package in.kalaiselvan.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityBackendApplication.class, args);
+	}
+
+}
---
+++ spring-security-backend/src/test/java/in/kalaiselvan/springsecurity/SpringSecurityBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package in.kalaiselvan.springsecurity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringSecurityBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
